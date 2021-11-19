34e8c699e15560560a78f5ac311c492424d3eccc
goreleaserbot <goreleaser@carlosbecker.com>
2021-11-18 20:11:17 -0500

    Brew formula update for labdoc version v0.1.3

--- labdoc.rb
+++ labdoc.rb
@@ -5,21 +5,21 @@
 class Labdoc < Formula
   desc "Generate README from a template, with some handy GitLab data"
   homepage "https://github.com/drewstinnett/labdoc"
-  version "0.1.2"
+  version "0.1.3"
   license "BSD-2-Clause"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/drewstinnett/labdoc/releases/download/v0.1.2/labdoc-0.1.2_macOS_arm64.tar.gz"
-      sha256 "bfaa22145e0a54c75de9c157767c80d62e8de846ab08e2f73171ef556fdd3800"
+      url "https://github.com/drewstinnett/labdoc/releases/download/v0.1.3/labdoc-0.1.3_macOS_arm64.tar.gz"
+      sha256 "738dc5dad5c943e0bb0cd609848ecdaeb64727d5322f94dbeaac38b690bde7be"
 
       def install
         bin.install "labdoc"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/drewstinnett/labdoc/releases/download/v0.1.2/labdoc-0.1.2_macOS_amd64.tar.gz"
-      sha256 "497383991ff71eb2e60789cc8d689efe89b0f94f34d36a604813c0deb155c6f1"
+      url "https://github.com/drewstinnett/labdoc/releases/download/v0.1.3/labdoc-0.1.3_macOS_amd64.tar.gz"
+      sha256 "5463a7b5fbf147e276260ac1641a1872e7bb251720ae3c7186cf9e88d1ae6f8e"
 
       def install
         bin.install "labdoc"
@@ -29,16 +29,16 @@ class Labdoc < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/drewstinnett/labdoc/releases/download/v0.1.2/labdoc-0.1.2_linux_amd64.tar.gz"
-      sha256 "0bea333fdb4e474abf2aba240aeb0bbd8a412ed11de3ded9ae2b8d0bc41474ea"
+      url "https://github.com/drewstinnett/labdoc/releases/download/v0.1.3/labdoc-0.1.3_linux_amd64.tar.gz"
+      sha256 "881a9deb8352f2ce3707bf5855036dd69c73510d549ffe201b1e108c18884905"
 
       def install
         bin.install "labdoc"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/drewstinnett/labdoc/releases/download/v0.1.2/labdoc-0.1.2_linux_arm64.tar.gz"
-      sha256 "52a109a6b3abbc96002f3bb79ea6e86bf90868934f638698949f7416b0311e99"
+      url "https://github.com/drewstinnett/labdoc/releases/download/v0.1.3/labdoc-0.1.3_linux_arm64.tar.gz"
+      sha256 "17f91c4a3bd01523dbded59c3326949bbb6e6c24e350e9db8c33416156dda937"
 
       def install
         bin.install "labdoc"
