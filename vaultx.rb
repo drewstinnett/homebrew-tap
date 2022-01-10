a81df97a65cf022988c15565cc79d4984f176865
goreleaserbot <goreleaser@carlosbecker.com>
2022-01-10 18:09:18 -0500

    Brew formula update for vaultx version v0.2.0

--- vaultx.rb
+++ vaultx.rb
@@ -5,21 +5,21 @@
 class Vaultx < Formula
   desc "Generate README from a template, with some handy GitLab data"
   homepage "https://github.com/drewstinnett/vaultx"
-  version "0.1.0"
+  version "0.2.0"
   license "BSD-2-Clause"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/drewstinnett/vaultx/releases/download/v0.1.0/vaultx-0.1.0_macOS_amd64.tar.gz"
-      sha256 "622967b281156ae66fee15d1076d66c71cfea20ddf92f7654b39a90560428b07"
+      url "https://github.com/drewstinnett/vaultx/releases/download/v0.2.0/vaultx-0.2.0_macOS_amd64.tar.gz"
+      sha256 "a20e662f2d70eaee9639a2564295d49a56272faaab803ddb2b70af063ed5cf3a"
 
       def install
         bin.install "vaultx"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/drewstinnett/vaultx/releases/download/v0.1.0/vaultx-0.1.0_macOS_arm64.tar.gz"
-      sha256 "dc4ca4ad0a3c3e1ce6497859728ea3c15137bfad328f4f7869f05bbd9f87ce0b"
+      url "https://github.com/drewstinnett/vaultx/releases/download/v0.2.0/vaultx-0.2.0_macOS_arm64.tar.gz"
+      sha256 "fb425c844d22fa40e47ba42fbb51a320859ca66fde5be4e00d0a13ebf65f5753"
 
       def install
         bin.install "vaultx"
@@ -28,17 +28,17 @@ class Vaultx < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/drewstinnett/vaultx/releases/download/v0.1.0/vaultx-0.1.0_linux_arm64.tar.gz"
-      sha256 "ee7b35916116865d82b1ee66cc47f23b570a22ffa39e3f28fe6eccb081e854c7"
+    if Hardware::CPU.intel?
+      url "https://github.com/drewstinnett/vaultx/releases/download/v0.2.0/vaultx-0.2.0_linux_amd64.tar.gz"
+      sha256 "9e8ef20187457d55e8702245916043299f3745ab4766122d131a606bcaddf440"
 
       def install
         bin.install "vaultx"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/drewstinnett/vaultx/releases/download/v0.1.0/vaultx-0.1.0_linux_amd64.tar.gz"
-      sha256 "ab714f769c5a268d0532c0103b51415d88b0fcad3ac29a973b6a9b404e47a177"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/drewstinnett/vaultx/releases/download/v0.2.0/vaultx-0.2.0_linux_arm64.tar.gz"
+      sha256 "a2650551cea04542c9fe555ad66f7cd7ad17c0c1028cf9144fd4e9c199a34f6d"
 
       def install
         bin.install "vaultx"
